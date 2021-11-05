15989a9c546cc3fa6f9bcd61938a4fbdb30afdb0
abltcy <bilgi@ahmetbaltaci.com>
2021-11-05 09:13:23 +0000

    Styled Components

--- template/screens/TestScreen/index.tsx
+++ template/screens/TestScreen/index.tsx
@@ -19,7 +19,18 @@ import {
 import CrossIcon from 'assets/svg/icons/Cross.svg';
 import CrossBlueIcon from 'assets/svg/icons/CrossBlue.svg';
 import HomeIcon from 'assets/svg/icons/Home.svg';
-import {resizeHeight, resizeWidth} from "../../common/constants";
+import {resizeHeight, resizeWidth} from 'common/constants';
+import {
+  StyledContainer,
+  StyledButtonText,
+  StyledScrollView,
+  StyledButton,
+  StyledHeader,
+  StyledHeaderText,
+  StyledRowContainer,
+  StyledText,
+  StyledTextContainer,
+} from './styles';
 
 export type TestScreenType = {
   navigation?: any;
@@ -32,93 +43,45 @@ export const TestScreen = ({navigation, route}: TestScreenType) => {
   const [incrementAmount, setIncrementAmount] = useState('2');
   const incrementValue = Number(incrementAmount) || 0;
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Redux test</Text>
-        </View>
-        <View style={styles.rowContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => dispatch(decrement())}>
-            <Text style={styles.buttonText}>Decrement value</Text>
-          </TouchableOpacity>
-          <View style={styles.textContainer}>
-            <Text style={styles.text}>{count}</Text>
-          </View>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => dispatch(increment())}>
-            <Text style={styles.buttonText}>Increment value</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+    <StyledScrollView>
+      <StyledContainer>
+        <StyledHeader>
+          <StyledHeaderText>Redux test</StyledHeaderText>
+        </StyledHeader>
+        <StyledRowContainer>
+          <StyledButton onPress={() => dispatch(decrement())}>
+            <StyledButtonText>Decrement value</StyledButtonText>
+          </StyledButton>
+          <StyledTextContainer>
+            <StyledText>{count}</StyledText>
+          </StyledTextContainer>
+          <StyledButton onPress={() => dispatch(increment())}>
+            <StyledButtonText>Increment value</StyledButtonText>
+          </StyledButton>
+        </StyledRowContainer>
+      </StyledContainer>
 
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Navigation test</Text>
-        </View>
-        <View style={styles.rowContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('Main')}>
-            <Text style={styles.buttonText}>Open Rn Page</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <StyledContainer>
+        <StyledHeader>
+          <StyledHeaderText>Navigation test</StyledHeaderText>
+        </StyledHeader>
+        <StyledRowContainer>
+          <StyledButton onPress={() => navigation.navigate('Main')}>
+            <StyledButtonText>Open Rn Page</StyledButtonText>
+          </StyledButton>
+        </StyledRowContainer>
+      </StyledContainer>
 
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Svg test</Text>
-        </View>
-        <View style={styles.rowContainer}>
+      <StyledContainer>
+        <StyledHeader>
+          <StyledHeaderText>Svg test</StyledHeaderText>
+        </StyledHeader>
+        <StyledRowContainer>
           <CrossBlueIcon width={resizeWidth(17)} height={resizeHeight(17)} />
           <CrossIcon width={resizeWidth(17)} height={resizeHeight(17)} />
           <HomeIcon width={resizeWidth(17)} height={resizeHeight(17)} />
-        </View>
-      </View>
-    </ScrollView>
+        </StyledRowContainer>
+      </StyledContainer>
+    </StyledScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    borderRadius: 10,
-    borderWidth: 1,
-    margin: 5,
-    padding: 5,
-  },
-  header: {
-    justifyContent: 'center',
-  },
-  headerText: {
-    color: 'black',
-    textAlign: 'center',
-  },
-  rowContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  button: {
-    padding: 5,
-    backgroundColor: 'black',
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 10,
-  },
-  buttonText: {
-    color: 'white',
-  },
-  textContainer: {
-    padding: 5,
-    backgroundColor: 'gray',
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 10,
-  },
-  text: {
-    color: 'white',
-    textAlign: 'center',
-  },
-});
---
+++ template/screens/TestScreen/styles.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components/native';
+
+export const StyledScrollView = styled.ScrollView``;
+
+export const StyledContainer = styled.View`
+  justify-content: center;
+  border-radius: 10px;
+  border-width: 1px;
+  margin: 5px;
+  padding: 5px;
+`;
+
+export const StyledHeader = styled.View`
+  justify-content: center;
+`;
+
+export const StyledHeaderText = styled.Text`
+  color: black;
+  text-align: center;
+`;
+
+export const StyledRowContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  padding: 5px;
+  background-color: black;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+`;
+
+export const StyledButtonText = styled.Text`
+  color: white;
+`;
+
+export const StyledTextContainer = styled.View`
+  padding: 5px;
+  background-color: gray;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+`;
+
+export const StyledText = styled.Text`
+  color: white;
+  text-align: center;
+`;
